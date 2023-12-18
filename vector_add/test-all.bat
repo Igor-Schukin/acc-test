1e0f85618cd407e1f37a34ef608a5372dce064d9
Igor Schukin <igor@df.rtu.lv>
2023-12-18 11:56:28 +0200

    Timestamp on test logs

--- vector_add/test-all.bat
+++ vector_add/test-all.bat
@@ -1,8 +1,8 @@
 @echo off
 
-echo ----- Start > test-all.txt
+echo ----- Start  %date% %time% > test-all.txt
 for %%v in (0S 1S 2S 3S 4S 0D 1D 2D 3D 4D VA) do (
     echo ----- %%v ----- >> test-all.txt
     .\bin\vector_add_%%v.exe %1 >> test-all.txt
 )
-echo ----- Finish >> test-all.txt
+echo ----- Finish %date% %time% >> test-all.txt
