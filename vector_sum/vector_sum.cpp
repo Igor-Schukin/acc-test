c16480a2d31f361a8c2f5ccc8380cbb18ebab0a0
Igor Schukin <igor@df.rtu.lv>
2024-01-04 14:23:27 +0200

    Fixed vesctor_sum

--- vector_sum/vector_sum.cpp
+++ vector_sum/vector_sum.cpp
@@ -19,28 +19,38 @@ int main(int argc, char *argv[])
     //~~~ Define vector size
 
     size_t size = MAX_COUNT;
+    int count = 1;
     if (argc > 1) size = std::stoull(argv[1]);
+    if (argc == 3) count = std::stoi(argv[2]);
     size = (size / BLOCK_SIZE) * BLOCK_SIZE;
     
     printf("vector size: %'ld\n", size);
 
-    //~~~ ACC payload
-
     type* __restrict__ a = new type[size];
     for (size_t i = 0; i < size; i++) a[i] = i;
+    res_type error = 0;
 
-    auto start = std::chrono::system_clock::now();
+    printf("elapsed time (ms) ");
 
-    //~~~ Include payload circles
+    for (int i = 0; i < count; i++) {
+        //~~~ ACC payload
 
-    printf("error: %llu\n", vector_sum(size, a) - (size-1) * size / 2); // res_type = unsigned long long int
+        auto start = std::chrono::system_clock::now();
 
-    //~~~ end of ACC payload
+        //~~~ Include payload circles
 
-    auto end = std::chrono::system_clock::now();
-    auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+        res_type res = vector_sum(size, a) - (size-1) * size / 2;
+        if (res > error) error = res;
 
-    printf("elapsed time %'ld ms\n", elapsed);
+        //~~~ end of ACC payload
+
+        auto end = std::chrono::system_clock::now();
+        auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+
+        printf("%'ld ", elapsed);
+    }
+
+    printf("\nerror: %llu\n", error); // res_type = unsigned long long int
 
     delete[] a;
 
--- vector_sum/vector_sum_4S.cpp
+++ vector_sum/vector_sum_4S.cpp
@@ -5,7 +5,7 @@
 res_type vector_sum(const size_t size, type* __restrict__ a)
 {
     res_type sum = 0;
-    #pragma acc data create(a[0:size])
+    #pragma acc data copy(a[0:size])
     {
         #pragma acc parallel loop reduction(+:sum)
         for (size_t i = 0; i < size; i++) sum += a[i];
