c247d82afd1fcffedf6aa831c6840b59539eeb47
Igor Schukin <igors@moderro.com>
2024-01-06 21:15:56 +0200

    Tests on home computer

--- vector_sum/test-all.bat
+++ vector_sum/test-all.bat
@@ -1,7 +1,7 @@
 @echo off
 
 echo ----- Start  %date% %time% > test-all.txt
-for %%v in (0S 1S 2S 3S 4S 0D 1D 2D 3D 4D VA) do (
+for %%v in (0S 1S 2S 3S 4S 0D 1D 2D 3D 4D) do (
     echo ----- %%v ----- >> test-all.txt
     .\bin\vector_sum_%%v.exe %1 %2 >> test-all.txt
 )
